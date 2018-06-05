1affb4610f3e7efaeba59d10597ff55b62a56d74
Josue V. Alorro <joalorro@gmail.com>
2018-06-05 21:22:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {'Frank Ocean': 'Pink & White'}
 
 function updatePlaylist(pL, artistName, songTitle) {
   
-  pL.artistname = songTitle;
+  pL[artistname] = songTitle;
   return pL
   
 }
